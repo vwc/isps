c0a4685bcd5ff0965adaf0f955a0de8c9ee7ff8d
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-07-20 17:40:40 +0200

    Update project type schema and styles

--- src/isps.sitecontent/isps/sitecontent/project.py
+++ src/isps.sitecontent/isps/sitecontent/project.py
@@ -27,6 +27,62 @@ class IProject(form.Schema, IImageScaleTraversable):
     """
     A specific project content type
     """
+    image = NamedBlobImage(
+        title=_(u"Preview Image"),
+        description=_(u"Upload main project image. A scaled version will be "
+                      u"used in search results and project listings"),
+        required=True,
+    )
+    text = RichText(
+        title=_(u"Main Text"),
+        description=_(u"Enter project description inluding project details"),
+        required=True
+    )
+    form.fieldset(
+        'details',
+        label=_(u"Project Details"),
+        fields=['contractor', 'architect', 'planning_period',
+                'construction_period', 'construction_costs', 'floor_space',
+                'floor_space_index', 'capacity', 'services']
+    )
+    contractor = schema.TextLine(
+        title=_(u"Building Contractor"),
+        required=True,
+    )
+    architect = schema.TextLine(
+        title=_(u"Architect"),
+        required=True,
+    )
+    planning_period = schema.TextLine(
+        title=_(u"Planning Period"),
+        required=False,
+    )
+    construction_period = schema.TextLine(
+        title=_(u"Construction Period"),
+        required=False
+    )
+    construction_costs = schema.TextLine(
+        title=_(u"Construction Costs"),
+        required=False,
+    )
+    floor_space = schema.TextLine(
+        title=_(u"Floor Space"),
+        required=False,
+    )
+    floor_space_index = schema.TextLine(
+        title=_(u"Floor Space Index"),
+        required=False,
+    )
+    capacity = schema.TextLine(
+        title=_(u"Cubic Capacity"),
+        required=False,
+    )
+    services = schema.Text(
+        title=_(u"ISP Services"),
+        description=_(u"Enter ISP services one sentence per line. These will "
+                      u"displayed as a bulleted list"),
+        required=False,
+    )
 
 
 class Project(dexterity.Container):
