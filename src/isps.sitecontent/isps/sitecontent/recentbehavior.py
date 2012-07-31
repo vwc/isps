fde47de1d34aaa5cda677563f3d0e333ec9e3734
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-07-31 18:50:41 +0200

    Basic version of a recent item behavior (needs to be generalized)

---
+++ src/isps.sitecontent/isps/sitecontent/recentbehavior.py
@@ -0,0 +1,45 @@
+from five import grok
+from zope import schema
+from zope.interface import Interface
+from zope.interface import alsoProvides
+from plone.directives import form
+from plone.autoform.interfaces import IFormFieldProvider
+from zope.lifecycleevent.interfaces import IObjectModifiedEvent
+
+from isps.sitecontent.contentpage import IContentPage
+
+from isps.sitecontent import MessageFactory as _
+
+
+class IRecentMarker(Interface):
+    """ Interface to mark content pages as recent """
+
+
+class IRecentEventBehavior(form.Schema):
+    """ Marker interface and fieldset for extending a content page with
+        dates metadata
+    """
+    form.fieldset(
+        'recentevent',
+        label=_(u"Recent Event Behavior"),
+        fields=['recent', 'release_date']
+    )
+    recent = schema.Bool(
+        title=_(u"Mark as recent event"),
+        description=_(u"Enable recent event behavior to list this item as "
+                      u"current or recent event/project"),
+        required=False,
+    )
+    release_date = schema.Datetime(
+        title=_(u"Release Date"),
+        description=_(u"Enter a date to provide chronological order for "
+                      u"recent events and projects"),
+        required="False",
+    )
+
+alsoProvides(IRecentEventBehavior, IFormFieldProvider)
+
+
+@grok.subscribe(IContentPage, IObjectModifiedEvent)
+def printMessage(obj, event):
+    print "Received event for", obj, "added to", event.newParent
