48a809e762196ba01e660dc83d4cebbf0746269b
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-08-13 18:18:21 +0200

    Add custom main navigation viewlet
    Pull the navtree from a vaigation strategy
    Animate megamenu 2nd level via jQuery (experimental)

--- src/isps.sitetheme/isps/sitetheme/resources/js/main-ck.js
+++ src/isps.sitetheme/isps/sitetheme/resources/js/main-ck.js
@@ -1,6 +1,14 @@
 /*jslint white:false, onevar:true, undef:true, nomen:true, eqeqeq:true, plusplus:true, bitwise:true, regexp:true, newcap:true, immed:true, strict:false, browser:true *//*global jQuery:false, document:false, window:false, location:false */(function(e) {
     e(document).ready(function() {
         if (jQuery.browser.msie && parseInt(jQuery.browser.version, 10) < 7) return;
+        var t, n = e("ul.megamenu");
+        e("a.megamenu-toggle").on("click", function(r) {
+            var i = e(this).data("appui-target");
+            t = e(i);
+            n.not(t).fadeOut("slow");
+            t.toggle("slow");
+            r.preventDefault();
+        });
         e(".bannertabs").tabs(".bannerbar > div.banner", {
             effect: "fade",
             fadeOutSpeed: 1e3,
