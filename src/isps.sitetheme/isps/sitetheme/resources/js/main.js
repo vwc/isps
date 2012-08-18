b874f1e415b5b09302f4253704a10352bd9c358c
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-08-18 16:14:03 +0200

    Refactor to include the new toolbar

--- src/isps.sitetheme/isps/sitetheme/resources/js/main.js
+++ src/isps.sitetheme/isps/sitetheme/resources/js/main.js
@@ -41,5 +41,27 @@
             $(".items img").removeClass("active");
             $(this).addClass("active");
         }).filter(":first").click();
+        $("a.popoverLogin").prepOverlay(
+        {
+            subtype: 'ajax',
+            filter: common_content_filter,
+            formselector: 'form#login_form',
+            noform: 'redirect',
+            redirect: function (overlay, responseText) {
+                var href = location.href;
+                if (href.search(/pwreset_finish$/) >= 0) {
+                    return href.slice(0, href.length-14) + 'logged_in';
+                } else {
+                    // look to see if there has been a server redirect
+                    var newTarget = $("<div>").html(responseText).find("base").attr("href"); 
+                    if ($.trim(newTarget) && newTarget !== location.href) { 
+                        return newTarget; 
+                    }
+                    // if not, simply reload
+                    return href;
+                }
+            }
+        }
+    );
     });
 }(jQuery));
