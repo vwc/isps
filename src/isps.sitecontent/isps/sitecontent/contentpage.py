46939163b27615f59eb87958f5a6fa26d2d92543
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-10-11 18:56:40 +0200

    Pep8 cleanup

--- src/isps.sitecontent/isps/sitecontent/contentpage.py
+++ src/isps.sitecontent/isps/sitecontent/contentpage.py
@@ -2,22 +2,9 @@ from five import grok
 from plone.directives import dexterity, form
 from plone.indexer import indexer
 
-from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
-
 from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
+from plone.namedfile.field import NamedBlobImage
 from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
 
 from Products.CMFPlone.utils import safe_unicode
 
